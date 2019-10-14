d5497d8344338dfb5790576267cd7e303c35eedb
Damius Green <40849814+Iforgedit@users.noreply.github.com>
2019-10-14 12:04:44 -0400

    Updates

---
+++ precache-manifest.41ddbf64c6828f9dc408323c5a187d9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5daf60daff31c14892505359cc65eb5",
+    "url": "/mugs/index.html"
+  },
+  {
+    "revision": "06462dbcb298d865c3b7",
+    "url": "/mugs/static/css/main.6587be9e.chunk.css"
+  },
+  {
+    "revision": "300a5debdc976e5bf713",
+    "url": "/mugs/static/js/2.25b1c4fe.chunk.js"
+  },
+  {
+    "revision": "06462dbcb298d865c3b7",
+    "url": "/mugs/static/js/main.1fac43c6.chunk.js"
+  },
+  {
+    "revision": "b6f4ce74f0653b8f5e19",
+    "url": "/mugs/static/js/runtime~main.735046dd.js"
+  }
+]);
